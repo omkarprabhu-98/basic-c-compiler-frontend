01d949d48122ccbb105ac79e28b0f61720746f2b
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2019-01-12 15:45:47 +0530

    0-lexical-analyzer: token types init file added

---
+++ 0-lexical-analyzer/token-types.h
@@ -0,0 +1,29 @@
+/**
+ * Token types used for lexems identified to be inserted into the Symbol table
+*/
+enum keywords
+{
+  INT=100
+};
+
+enum identifiers
+{
+  ID=200
+};
+
+enum punctuators
+{
+  DELIMITER=200
+};
+
+enum operators
+{
+  INCRECEMENT=300
+};
+
+enum constants
+{
+  INT_CONST=400,
+  STRING_CONST
+};
+
