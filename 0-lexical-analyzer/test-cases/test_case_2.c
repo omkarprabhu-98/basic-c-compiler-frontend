e60f71b807e65a6e39519d06439b939fd97b2389
Edwin Thomas <edwinthomas444@gmail.com>
2019-01-16 19:22:55 +0400

    0-lexical-analyzer: Added identifers and some keywords test cases

---
+++ 0-lexical-analyzer/test-cases/test_case_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// main function of the program
+int main () {
+    
+
+    /**
+     * Various identifier declarations and initializations along with data types and 
+     * few keywords.
+     */
+     long int c;
+     printf("Identifier name is c with data type long int\n");
+     long long int _dc = 14;
+     printf("Identifier name is _dc with data type long int\n");
+     float x = 12;
+     printf("Identifier named x is initialized with value 12\n");
+     float a = b;
+     printf("Identifier named a is initialized with value of identifier named b\n");
+     short _foo_foo1 = 90;
+     printf("Identifier named _foo_foo1 is initialized with value 90\n");
+     unsigned int A_bit = 100;
+     printf("Identifier named A_bit is initialized with value 100\n");
+
+
+}
---
+++ 0-lexical-analyzer/test-cases/test_case_2_error.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// main function of the program
+int main () {
+    
+
+    /**
+     * Various identifier declarations and initializations along with data types and 
+     * few keywords.
+     */
+     long int 9c;
+     printf("9c is not a valid identifier\n");
+     long long int 91_DC = 14;
+     printf("91_DC is not a valid identifier\n");
+     
+}
