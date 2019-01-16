741a7a3be5f7848b017e8ad5a1240d5ac1c6094a
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2019-01-16 13:40:46 +0530

    0-lexical-analyzer: error cases added + whitespaces ignored

---
+++ 0-lexical-analyzer/test-cases/test_case_0_error.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// main function of the program
+int main () {
+    int x;
+
+    //ternary check
+    x = 2 > 5 != 1 ? 5 < 8 && 8 > 2 ? !5 ? 10 : 20 : 30 : 40;
+    //ternary check ends
+
+
+    x = 5 > 8 ? 10 : 1 != 2 < 5 ? 20 : 30; 
+
+
+    //ternary with errors below
+    x = 5 > 8 ? 10 : 1 != 2 < 5 ? 20 : 30 :;
+
+    x = 5 > 8 ? 10 ? 1 != 2 < 5 ? 20 : 30; 
+
+    int a @ 12;
+}
