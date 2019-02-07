c58e5e40357d030ee4d69268bdbfc6d670e2c0e1
dibyadarshan hota <dibyadarshan.hota@gmail.com>
2019-02-07 07:09:19 +0530

    Parser: More tests added

---
+++ 1-syntax-analyzer/test-cases/test_case_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// main function of the program
+int main () {
+  
+     int c;
+     float x;
+     unsigned A_bit;
+}
---
+++ 1-syntax-analyzer/test-cases/test_case_2_error.c
@@ -0,0 +1,11 @@
+// error 
+#include <stdio.h>
+
+// main function of the program
+int main () {
+     int 9c;
+     printf("9c is not a valid identifier\n");
+     int 91_DC;
+     printf("91_DC is not a valid identifier\n");
+     
+}
